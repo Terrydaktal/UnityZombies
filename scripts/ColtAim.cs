5203a51fe0879bbae05cccb26c0d39baa96f09ff
Terrydaktal <9lewis9@gmail.com>
2018-06-06 05:08:04 +0100

    Update ColtAim.cs
    updated for the new gun vibrate animation whilst aiming

--- scripts/ColtAim.cs
+++ scripts/ColtAim.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class ColtAim : MonoBehaviour {
     public GameObject FPSCamera;
     bool Aiming = false;
+    bool reloading = false;
+    bool aimingReady = false;
+
 
     // Use this for initialization
     void Start() {
         Aiming = false;
+        reloading = false;
+        aimingReady = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown("1") || Input.GetKeyDown("2")) && Aiming){
+        if ((Input.GetKeyDown("1") || Input.GetKeyDown("2")) && Aiming)
+        {
             FPSCamera.GetComponent<Animation>().Play("fastUnAimColt");
             Aiming = false;
+            reloading = false;
         }
 
         if (Input.GetButtonDown("Fire2"))
@@ -24,7 +31,32 @@ public class ColtAim : MonoBehaviour {
             if (!Aiming) FPSCamera.GetComponent<Animation>().Play("aimColt");
             else FPSCamera.GetComponent<Animation>().Play("unAimColt");
             Aiming = !Aiming;
+            aimingReady = false;
+            StartCoroutine(WaitingForAim());
+        }
+
+        if (Input.GetButtonDown("Fire1") && !reloading && aimingReady && Aiming)
+        {
+            FPSCamera.GetComponent<Animation>().Play("coltAimedVibration");
+
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            reloading = true;
+            StartCoroutine(WaitingForReload());
         }
     }
-  
+
+    IEnumerator WaitingForReload()
+    {
+        yield return new WaitForSeconds(3);
+        reloading = false;
+    }
+
+    IEnumerator WaitingForAim()
+    {
+        yield return new WaitForSeconds(0.2f);   //same time for aiming in and out
+        aimingReady = true;
+    }
 }
