f0c604aa89a3e50c90b466e4c9285ded86f44fdf
Terrydaktal <9lewis9@gmail.com>
2018-06-07 03:37:59 +0100

    Update animator.cs
    updated for zombie window barrier introduction

--- scripts/animator.cs
+++ scripts/animator.cs
@@ -1,17 +1,59 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class animator : MonoBehaviour {
     Animator controller;
+    int boards = 1;
+    NavMeshAgent nav;
+    RaycastHit hit;
+    public GameObject Mesh;
+    public GameObject SpawnWall;
+    public bool WaitingForNextBoard;
+
     // Use this for initialization
     void Start () {
         controller = GetComponentInParent<Animator>();
+        nav = GetComponentInParent<NavMeshAgent>();
+        WaitingForNextBoard = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Physics.Raycast(transform.position, Vector3.down, out hit)) {
+            hit.transform.SendMessage("RequestBoardUpdate", Mesh, SendMessageOptions.DontRequireReceiver);
+            print(hit.transform.tag);
+            if (hit.transform.tag == "SpawnWallZombieSide")
+            {
+                print(boards);
+                if (boards > 0 && !WaitingForNextBoard)
+                {
+                    WaitingForNextBoard = true;
+                    Invoke("RemoveBoard", 1.5f);
+                }
 
+                if (boards < 1)
+                {
+                    print("happens");
+                    nav.isStopped = false;
+                }
+            }
+        }
+
+    }
+
+    private void RemoveBoard()
+    {
+        print("boards > 0");
+        nav.isStopped = true;
+        SpawnWall.SendMessage("RemoveBoard", SendMessageOptions.DontRequireReceiver);
+        WaitingForNextBoard = false;
+    }
+
+    void ReceiveBoardAmount(int boardsRemaining)
+    {
+        boards = boardsRemaining;
     }
 
     void HitByRaycast(int damage)
