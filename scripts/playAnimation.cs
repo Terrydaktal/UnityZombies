03c419468e00c6fd3728371d760fb4e81fd7b269
Terrydaktal <9lewis9@gmail.com>
2018-06-08 14:56:28 +0100

    Update playAnimation.cs
    moved setactive to other script because it can't set itself active when diasabled

--- scripts/playAnimation.cs
+++ scripts/playAnimation.cs
@@ -2,28 +2,24 @@ using UnityEngine;
 using System.Collections;
 
 public class playAnimation : MonoBehaviour {
-	public bool play;
 	ParticleSystem[] dust;
+    public GameObject thisboard;
+    public AudioSource boardbreak;
 	// Use this for initialization
 	void Start () {
 		dust = GetComponentsInChildren<ParticleSystem> ();
-	}
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		if(play)
-		{
-            gameObject.SetActive(true);
-            dust[0].Play();
-			dust[1].Play();
-			play = false;
+	void EnableBoard() { 
+        dust[0].Play();
+        dust[1].Play();
 		}
-	}
 
-    void DisableBoard()
+void DisableBoard()
     {
         dust[0].Play();
         dust[1].Play();
-        gameObject.SetActive(false); //requires animation
+        boardbreak.Play();
+      //  thisboard.GetComponent<Renderer>().enabled = false;
     }
 }
