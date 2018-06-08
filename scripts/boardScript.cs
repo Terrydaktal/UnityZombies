e28c555d6d999d781597ce8c648121bc7d16ca64
Terrydaktal <9lewis9@gmail.com>
2018-06-08 15:04:42 +0100

    Update boardScript.cs
    tidy

--- scripts/boardScript.cs
+++ scripts/boardScript.cs
@@ -1,59 +1,50 @@
 using UnityEngine;
 using System.Collections;
 
-public class boardScript : MonoBehaviour {
+public class boardScript : MonoBehaviour
+{
     public int boards, previousBoards;
     public Animator[] boardAnim;
-    public GameObject board1;
-    public GameObject board2;
-    public GameObject board3;
-    public GameObject board4;
-    public GameObject board5;
-    public GameObject board6;
-    GameObject[] boardObjects = new GameObject[6];
+    public GameObject[] boardObjects;
 
-	// Use this for initialization
-	void Start () {
-		boardAnim = GetComponentsInChildren<Animator> ();
-        boardObjects[0] = board1;
-        boardObjects[1] = board2;
-        boardObjects[2] = board3;
-        boardObjects[3] = board4;
-        boardObjects[4] = board5;
-        boardObjects[5] = board6;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(boards !=previousBoards)
-		{
-			previousBoards = boards;
-			switch(boards)
-			{
+    // Use this for initialization
+    void Start()
+    {
+        boardAnim = GetComponentsInChildren<Animator>();
+    }
 
-            		case 1:
-				boardAnim[0].Play("boardAnimation1");
-                                return;
-			case 2:
-				boardAnim[1].Play("boardAnimation2");
-				return;
-			case 3:
-				boardAnim[2].Play("boardAnimation3");
-				return;
-			case 4:
-				boardAnim[3].Play("boardAnimation4");
-				return;
-			case 5:
-				boardAnim[4].Play("boardAnimation5");
-				return;
-			case 6:
-				boardAnim[5].Play("boardAnimation6");
-				return;
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (boards != previousBoards)
+        {
+            previousBoards = boards;
+            switch (boards)
+            {
 
-    void Acknowledge ()
+                case 1:
+                    boardAnim[0].Play("boardAnimation1");
+                    return;
+                case 2:
+                    boardAnim[1].Play("boardAnimation2");
+                    return;
+                case 3:
+                    boardAnim[2].Play("boardAnimation3");
+                    return;
+                case 4:
+                    boardAnim[3].Play("boardAnimation4");
+                    return;
+                case 5:
+                    boardAnim[4].Play("boardAnimation5");
+                    return;
+                case 6:
+                    boardAnim[5].Play("boardAnimation6");
+                    return;
+            }
+        }
+    }
+
+    void Acknowledge()
     {
         if (boards < 6) boards += 1;
         print("no boards : " + boards);
@@ -82,7 +73,7 @@ public class boardScript : MonoBehaviour {
     {
         boardObjects[boards - 1].SetActive(false);
         boards -= 1;
-    } 
+    }
 
 
 }
