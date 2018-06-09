0293fe9a183af92323999d662902e6dde08f6f75
Terrydaktal <9lewis9@gmail.com>
2018-06-09 03:13:35 +0100

    Update playAnimation.cs
    added new window building sound

--- scripts/playAnimation.cs
+++ scripts/playAnimation.cs
@@ -5,12 +5,14 @@ public class playAnimation : MonoBehaviour {
 	ParticleSystem[] dust;
     public GameObject thisboard;
     public AudioSource boardbreak;
+    public AudioSource windowbuild;
 	// Use this for initialization
 	void Start () {
 		dust = GetComponentsInChildren<ParticleSystem> ();
     }
 	
-	void EnableBoard() { 
+	void EnableBoard() {
+        windowbuild.Play();
         dust[0].Play();
         dust[1].Play();
 		}
