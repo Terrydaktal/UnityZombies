bd1a4fd05ff6283a07159e7f38d807f1311a9d7c
Terrydaktal <9lewis9@gmail.com>
2018-06-11 16:06:35 +0100

    Update LockRotation.cs
    now manages rain sounds

--- scripts/LockRotation.cs
+++ scripts/LockRotation.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class LockRotation : MonoBehaviour {
 
 	Quaternion rotation;
-	
+    public AudioSource rain;
+    public AudioSource raindrops;
+    public bool notPlaying = true;
 	void Awake()
 	{
 		rotation = transform.rotation;
@@ -14,4 +16,21 @@ public class LockRotation : MonoBehaviour {
 	{
 		transform.rotation = rotation;
 	}
+
+    void Update()
+    {
+        if (notPlaying)
+        {
+            notPlaying = false;
+           // rain.Play();
+            raindrops.Play();
+            StartCoroutine(PlaySound());
+        }
+    }
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(39);
+        notPlaying = true;
+    }
 }
