04fa73728ecf02e28422c5c9b3eb55f4d872c432
Terrydaktal <9lewis9@gmail.com>
2018-06-11 16:03:21 +0100

    Update ZombieController.cs
    tided up code and added zombie attack mechanic

--- scripts/ZombieController.cs
+++ scripts/ZombieController.cs
@@ -7,21 +7,11 @@ public class ZombieController : MonoBehaviour {
     NavMeshAgent nav;
     Transform player;
     Animator controller;
+    AudioSource[] audios;
     int localHealth = 100;
-    public AudioSource hitNoKill;
-    public AudioSource hitKill;
-    public AudioSource hitKill2;
-    public AudioSource zombieDying;
-    public AudioSource zombieDying2;
-    public AudioSource zombieDying3;
-    public AudioSource zombieDying4;
-    public AudioSource zombieDying5;
-    public AudioSource zombieDying6;
-    public AudioSource zombieDying7;
-    public AudioSource zombieDying8;
-    public AudioSource zombieDying9;
-    public AudioSource zombieDying10;
-    public AudioSource[] audios;
+    public bool attacking = false;
+    public bool death = false;
+    public GameObject FPSCamera;
 
     // Use this for initialization
     void Start() {
@@ -49,13 +39,39 @@ public class ZombieController : MonoBehaviour {
                 if (localHealth < 1)
                 {
                     audios[Random.Range(3, 11)].Play();
+                    death = true;
                 }
             } 
         }
-
+         print(Mathf.Abs(Vector3.Distance(player.position, this.transform.position)));
+         if (Mathf.Abs(Vector3.Distance(player.position, this.transform.position)) < 2.65 && !attacking && !death)
+          {
+            attacking = true;
+            controller.speed = 1.5f;
+            controller.Play("attack2");
+            audios[Random.Range(11, 20)].Play();
+            Invoke("CheckHit", 0.3f);
+            StartCoroutine(WaitForAttackEnd());
+         }
 
         // controller.SetFloat("speed", Mathf.Abs (nav.velocity.x) + Mathf.Abs(nav.velocity.z));
 
     }
 
+    IEnumerator WaitForAttackEnd()
+    {
+        yield return new WaitForSeconds(1.6f);
+        attacking = false;
+    }
+
+    void CheckHit()
+    {
+        if(Mathf.Abs(Vector3.Distance(player.position, this.transform.position)) < 3.5 && !death)
+        {
+            controller.speed = 1f;
+            PlayerManager pm = FPSCamera.GetComponent<PlayerManager>();
+            pm.health -= 49;
+
+        }
+    }
 }
